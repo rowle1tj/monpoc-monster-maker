e92ff6017c1eddea94a12854311b36b5aae097f8
Tim Rowley <rowle1tj@gmail.com>
2022-10-19 23:27:01 -0400

    Added first bit of interactivity to the monster maker

--- js/classes/DiceNumberIcon.js
+++ js/classes/DiceNumberIcon.js
@@ -16,27 +16,39 @@ function DiceNumberIcon(number, height, bgColor, txtColor) {
     } else {
         this.txtColor = c.COLORS.BLACK;
     }
+
+    this.backgroundShape = null;
+    this.textObject = null;
     
     this.setup();
+    var _this = this;
+    this.addEventListener("click", function(event) { 
+        var newNumber = prompt("New Value?");
+        _this.number = newNumber;
+        _this.removeChild(_this.backgroundShape);
+        _this.removeChild(_this.textObject);
+        _this.setup();
+        _this.stage.update();
+    });
 }
 var p = createjs.extend(DiceNumberIcon, createjs.Container);
 
 
 p.setup = function() {
-    var background = new createjs.Shape();
-    background.graphics
+    this.backgroundShape = new createjs.Shape();
+    this.backgroundShape.graphics
         .beginFill(this.bgColor)
         .setStrokeStyle(c.BOX_BORDER_STROKE)
         .beginStroke(c.COLORS.BLACK)
         .drawPolyStar(0, 0, this.height, 6, 0, -90);
-    this.addChild(background);
+    this.addChild(this.backgroundShape);
 
-    var txt = c.createText("38px Teko", this.number);
-    var b = txt.getBounds();
-    txt.color = this.txtColor;
-    txt.regX = b.width / 2;
-    txt.regY = b.height / 2;
-    this.addChild(txt);
+    this.textObject = c.createText("38px Teko", this.number);
+    var b = this.textObject.getBounds();
+    this.textObject.color = this.txtColor;
+    this.textObject.regX = b.width / 2;
+    this.textObject.regY = b.height / 2;
+    this.addChild(this.textObject);
 };
 
 DiceNumberIcon = createjs.promote(DiceNumberIcon, "Container");
